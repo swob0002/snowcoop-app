cc9cb7bfffa50bfa3da8234acf866b908b28ed8f
Malcolm Swoboda <swob0002@algonquinlive.com>
2019-04-11 13:46:45 -0400

    testing add-address branch

---
+++ src/services/address.service.js
@@ -0,0 +1,24 @@
+// Use Axios to make a POST request to server API
+// import Axios from 'axios'
+
+// const AddressService = {
+//     getAddressList() {
+//         return Axios
+//         .get('http://localhost:3000/address')
+//         .then(async response => {
+//           if (response.status === 200 || response.status === 201) {
+//             const { payload } = response.data
+//             const addresses = payload.map(address => {
+//                 address.lat = Number(address.lat);
+//                 address.lng = Number(address.lng);
+//                 return address
+//             }
+//             return addresses
+//           }
+//     }
+
+//     })
+// };
+// export default AddressService
+
+// test
